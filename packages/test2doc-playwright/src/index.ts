8468f3e2c53c1668d4b1e91fbd8ee58a4c1da876
dethstrobe <dethstrobe@gmail.com>
2025-06-13 18:46:07 -0400

    test implementation of test2doc

--- packages/test2doc-playwright/src/index.ts
+++ packages/test2doc-playwright/src/index.ts
@@ -1,3 +1,23 @@
-export const hello = () => {
-  return 'Hello, world!';
+import type {
+  FullConfig, FullResult, Reporter, Suite, TestCase, TestResult
+} from '@playwright/test/reporter';
+
+class MyReporter implements Reporter {
+  onBegin(config: FullConfig, suite: Suite) {
+    console.log(`Starting the run with ${suite.allTests().length} tests`, config.version);
+  }
+
+  onTestBegin(test: TestCase, result: TestResult) {
+    console.log(`Starting test ${test.title}`, result.status);
+  }
+
+  onTestEnd(test: TestCase, result: TestResult) {
+    console.log(`Finished test ${test.title}: ${result.status}`);
+  }
+
+  onEnd(result: FullResult) {
+    console.log(`Finished the run: ${result.status}`);
+  }
 }
+
+export default MyReporter;
