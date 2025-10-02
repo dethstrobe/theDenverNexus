da37ce78c883c95129084e226f807f51b7bb5859
dethstrobe <dethstrobe@gmail.com>
2025-10-02 11:56:57 -0600

    fix(test2doc-playwright): increase threshold when detecting screenshot changes

--- packages/test2doc-playwright/tests/screenshot-integration.spec.ts
+++ packages/test2doc-playwright/tests/screenshot-integration.spec.ts
@@ -88,7 +88,7 @@ const expectScreenshotToMatch = async (
       diff.data,
       width,
       height,
-      { threshold: 0.1 },
+      { threshold: 0.5 },
     )
     if (matchedPixels > 0) {
       writeFileSync(
