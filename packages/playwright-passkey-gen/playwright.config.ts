591784cfc5d5b1969aa7f39e4264cf7360165ae5
dethstrobe <dethstrobe@gmail.com>
2026-01-22 14:13:34 -0700

    fix(playwright-passkey-gen): addbuild step to update test server for e2e tests

--- packages/playwright-passkey-gen/playwright.config.ts
+++ packages/playwright-passkey-gen/playwright.config.ts
@@ -75,6 +75,6 @@ export default defineConfig({
   webServer: {
     command: "pnpm start",
     url: "http://localhost:5173",
-    reuseExistingServer: !process.env["CI"],
+    reuseExistingServer: false, // Always restart to pick up new test passkey
   },
 })
