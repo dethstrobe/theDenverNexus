ef461d9ef114393a5898c30bb167e852c3adf5ff
dethstrobe <dethstrobe@gmail.com>
2026-02-16 12:47:55 -0700

    fix(test2doc-docs): fix User-declared canonical url

--- apps/test2doc-docs/docusaurus.config.ts
+++ apps/test2doc-docs/docusaurus.config.ts
@@ -20,7 +20,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: "https://test2doc.com",
+  url: "https://www.test2doc.com",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
